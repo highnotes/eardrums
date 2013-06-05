7b61a23a7da155d0955009db55d90199fd787172
Subhash Bhushan <subhash.bhushan@gmail.com>
2013-06-05 23:40:00 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eardrums::Application.config.secret_key_base = '459edb424c86e3ad95ed8d6c0242867f4daeae4524cd7584fd55f939b4650bc957b64e6fee0dfa046ecaa25a295caa90b47392dccaf9b50ba429f10c1784a4af'
