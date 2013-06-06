470d34fd319356662675c2042762bd0f7747dd0c
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2013-06-06 22:44:10 +0530

    Add Static Home page, with Coming Soon message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Eardrums::Application.routes.draw do
+  root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
