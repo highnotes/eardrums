8a59b58eeb5b99fa68f01c8eebef340456097dae
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2013-06-30 19:36:58 +0530

    Update without current password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,41 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
+  def update
+    @user = User.find(current_user.id)
+
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(devise_parameter_sanitizer.for(:account_update))
+    else
+      # remove the virtual current_password attribute update_without_password
+      # doesn't know how to ignore it
+      params[:user].delete(:current_password)
+      @user.update_without_password(devise_parameter_sanitizer.for(:account_update))
+    end
+
+    if successfully_updated
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:first_name, :last_name,
-        :email, :password, :password_confirmation)
+      u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:first_name, :last_name,
-        :email, :password, :password_confirmation, :current_password)
+      u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
     end
+  end
+  
+  private
+  
+  def needs_password?(user, params)
+    !params[:user][:password].blank?
   end
 end
