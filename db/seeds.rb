69cc6b428bd7ce205ee992fec93b46b0fa5c0a5a
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2013-07-17 13:32:36 +0530

    Clean up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,9 @@ User.create! do |r|
   r.email       = 'admin@eardrums.in'
   r.password    = 'password'
   r.role        = 'admin'
+  r.username    = 'admin'
 end
     
-Discipline.delete_all
-disciplines = Discipline.create([{name: 'Guitar', description: 'Guitar'}, 
-                                 {name: 'Hindustani', description: 'Hindustani'},
-                                 {name: 'Keyboard', description: 'Keyboard'},
-                                 {name: 'Carnatic', description: 'Carnatic'}])
-
 Level.delete_all                                 
 levels = Level.create([{name: 'Early Elementary', description: 'Early Elementary'}, 
               {name: 'Elementary', description: 'Elementary'}, 
@@ -31,8 +26,3 @@ levels = Level.create([{name: 'Early Elementary', description: 'Early Elementary
               {name: 'Advanced', description: 'Advanced'}, 
               {name: 'Advanced +', description: 'Advanced +'},
               {name: 'Professional', description: 'Professional'}])
-
-Course.delete_all
-courses = Course.create([{name: "Beginner - Carnatic", description: "Beginner - Carnatic", duration: 8, price: "1500", status: "Active", level: levels[2], discipline: disciplines[3]},
-               {name: "Beginner - Guitar", description: "Beginner - Guitar", duration: 8, price: "1500", status: "Active", level: levels[2], discipline: disciplines.first},
-               {name: "Beginner - Keyboard", description: "Beginner - Keyboard", duration: 8, price: "1500", status: "Active", level: levels[2], discipline: disciplines[2]}])
