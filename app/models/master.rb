271777b4f8caa88974d2b9613c7162c7a372b3f2
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2013-08-12 15:03:24 +0530

    Add field conversions before save to Masters Entity

--- app/models/master.rb
+++ app/models/master.rb
@@ -7,7 +7,18 @@ class Master < ActiveRecord::Base
   validates_numericality_of :index, only_integer: true
   validates_uniqueness_of :index, scope: :group
   
+  before_save :format_group, :format_name
+  
   def self.get_group_collection(group)
     Master.where(group: group).order('"masters"."group" ASC, index ASC').to_a
   end
+  
+  private
+    def format_group
+      self.group = group.gsub(/ /, '_').upcase if self.group.present?
+    end
+  
+    def format_name
+      self.name = name.gsub(/ /, '_').upcase if self.name.present?
+    end
 end
--- spec/models/master_spec.rb
+++ spec/models/master_spec.rb
@@ -14,6 +14,38 @@ describe Master do
   it { should validate_numericality_of(:index).only_integer }
   it { should validate_uniqueness_of(:index).scoped_to(:group) }
   
+  context "Before Save" do
+    it "should replace spaces in group with underscore" do
+      @master_with_group_in_spaces = @master.dup
+      @master_with_group_in_spaces.group = "INFORMATION SOURCES"
+      @master_with_group_in_spaces.save
+      expect(@master_with_group_in_spaces.group).to eq("INFORMATION_SOURCES")
+      @master_with_group_in_spaces.destroy
+    end
+    
+    it "should convert group to uppercase" do
+      @master_with_group_in_lowercase = @master.dup
+      @master_with_group_in_lowercase.group = "information"
+      @master_with_group_in_lowercase.save
+      expect(@master_with_group_in_lowercase.group).to eq("INFORMATION")
+    end
+    
+    it "should replace spaces in name with underscore" do
+      @master_with_name_in_spaces = @master.dup
+      @master_with_name_in_spaces.name = "MEMBER REFERRAL"
+      @master_with_name_in_spaces.save
+      expect(@master_with_name_in_spaces.name).to eq("MEMBER_REFERRAL")
+      @master_with_name_in_spaces.destroy
+    end
+    
+    it "should convert name to uppercase" do
+      @master_with_name_in_lowercase = @master.dup
+      @master_with_name_in_lowercase.name = "radio"
+      @master_with_name_in_lowercase.save
+      expect(@master_with_name_in_lowercase.name).to eq("RADIO")
+    end
+  end
+  
   context "Collections" do
     it "should return collections of a group" do
       @master_dup = @master.dup
