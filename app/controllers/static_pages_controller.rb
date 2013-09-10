ea5ea9e2122f5b02bc6ca457085fcea7de182c4a
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2013-09-10 13:04:37 +0530

    Tweak Home Page - Phase 1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   skip_before_filter :authenticate_user!
   
   def home
+    ids = Course.where(index: 1).pluck(:id).shuffle[0..3]
+    @courses = Course.where(id: ids).shuffle
   end
   
   def terms
